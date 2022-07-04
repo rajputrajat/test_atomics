9626c746a8894cbbb254690ba7141f98a3035741
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2022-07-04 22:14:18 +0530

    something happened

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,31 @@ fn main() {
     let b = Arc::new(Mutex::new(20));
     let c = Arc::new(Mutex::new(30));
 
-    let aa = Arc::clone(&a);
-    let bb = Arc::clone(&b);
-    let cc = Arc::clone(&c);
+    for _ in 0..1_000_000 {
+        let aa = Arc::clone(&a);
+        let bb = Arc::clone(&b);
+        let cc = Arc::clone(&c);
 
-    thread::spawn(move || {
-        *aa.lock().unwrap() = 30;
-        if *bb.lock().unwrap() == 30 {
-            *cc.lock().unwrap() = 20;
-        }
-    });
+        thread::spawn(move || {
+            *aa.lock().unwrap() = 30;
+            if *bb.lock().unwrap() == 30 {
+                *cc.lock().unwrap() = 20;
+            }
+        });
 
-    let aa = Arc::clone(&a);
-    let bb = Arc::clone(&b);
-    let cc = Arc::clone(&c);
+        let aa = Arc::clone(&a);
+        let bb = Arc::clone(&b);
+        let cc = Arc::clone(&c);
 
-    thread::spawn(move || {
-        *bb.lock().unwrap() = 30;
-        if *aa.lock().unwrap() == 10 {
-            *cc.lock().unwrap() = 20;
-        }
-    });
+        thread::spawn(move || {
+            *bb.lock().unwrap() = 30;
+            if *aa.lock().unwrap() == 10 {
+                *cc.lock().unwrap() = 20;
+            }
+        });
 
-    println!(
-        "{}, {}, {}",
-        *a.lock().unwrap(),
-        *b.lock().unwrap(),
-        *c.lock().unwrap()
-    );
+        assert_eq!(*a.lock().unwrap(), 10);
+        assert_eq!(*b.lock().unwrap(), 20);
+        assert_eq!(*c.lock().unwrap(), 30);
+    }
 }
